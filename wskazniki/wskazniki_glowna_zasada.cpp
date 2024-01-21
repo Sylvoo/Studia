dd7730200eee1542026e579c2e64657bdce9267a
Sylwester Slusarczyk <Sylwek@wlasciciel.com>
2024-01-22 00:32:44 +0100

    wszystko

--- wskazniki/wskazniki_glowna_zasada.cpp
+++ wskazniki/wskazniki_glowna_zasada.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {   
     int liczba = 144;
-    int *w;
-    w = &liczba;
+    int *w = &liczba;
+    //w = &liczba;
     cout<< liczba <<endl; //144
     cout<< *w <<endl; // 144
     cout<< w <<endl; // 0xe8641ffbf4
